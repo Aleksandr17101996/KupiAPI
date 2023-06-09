90e595141b183f360b89b3c7a440350dae071231
Aleksandr17101996 <poleshuk.alex3460@yandex.ru>
2023-06-09 22:27:06 +0300

    add locale

--- tests/account_tests.py
+++ tests/account_tests.py
@@ -1,20 +1,20 @@
 import random
 
 from api.account_api import Account
-from config import Currency
-
-curr = Currency()
+from config import Locale
 
 
 class TestAccount(Account):
-    cur_ru = Currency.CURRENCY_RU
+    currency = Locale.CURRENCY
+    lang = Locale.LANG
+    country = Locale.COUNTRY
 
     def test_post_sign_up_valid_user(self):
-        email = "poleshuk.alex134@gmail.com"
+        email = "poleshuk.alex@gmail.com"
         password = "aaaAAA1"
-        lang = "ru"
-        country = "RU"
-        currency = self.cur_ru[random.randint(0, 7)]
+        lang = self.lang[random.randint(0, 7)]
+        country = self.country[random.randint(0, 1)]
+        currency = self.currency[random.randint(0, 14)]
         status_code, body = self.post_sign_up(email, password, lang, country, currency)
         assert status_code == 200, "Статус код не соответсвует ожидаемому"
         assert body["status"] == "success", "Статус в теле ответа не соответсвует"
