375f86b01e9cfd5c7bece4ec279d923bf73358fe
Aleksandr17101996 <poleshuk.alex3460@yandex.ru>
2023-06-09 21:47:06 +0300

    add class Currency

---
+++ config.py
@@ -0,0 +1,8 @@
+class Base:
+    BASE_URL = "https://flights-api-orders.kupibilet.ru/"
+    VERSION = "v4/"
+
+
+class Currency:
+    CURRENCY_COM = ["USD", "EUR", "GBP", "AUD", "SGD", "CAD", "PLN", "AED"]
+    CURRENCY_RU = ["RUB", "USD", "KZT", "UAH", "UZS", "AZN", "KGS", "AMD"]
