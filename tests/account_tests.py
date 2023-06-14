130b459f4a81a610005b401f0cf34263b8cd6b9d
Aleksandr17101996 <poleshuk.alex3460@yandex.ru>
2023-06-14 23:32:12 +0300

    add validation

--- tests/account_tests.py
+++ tests/account_tests.py
@@ -3,12 +3,14 @@ import random
 from api.account_api import Account
 from config import Locale
 from data.generator import generated_persone, generated_password
+from data.validator import ValidateSingUp
 
 
 class TestAccount(Account):
     currency = Locale.CURRENCY
     lang = Locale.LANG
     country = Locale.COUNTRY
+    validate = ValidateSingUp()
 
     def test_post_sign_up_valid_user(self):
         person_info = next(generated_persone())
@@ -18,7 +20,7 @@ class TestAccount(Account):
         country = self.country[random.randint(0, 1)]
         currency = self.currency[random.randint(0, 14)]
         status_code, body = self.post_sign_up(email, password, lang, country, currency)
+        self.validate.sing_up_validation(body)
         assert status_code == 200, "Статус код не соответсвует ожидаемому"
         assert body["status"] == "success", "Статус в теле ответа не соответсвует"
         assert body["data"]["email"] == email, "Почта в теле ответа не соответсвует"
-        
